13689f72871cbfe731b73e9518b67cbe22a66992
EricWoodford <39199956+EricWoodford@users.noreply.github.com>
2019-05-29 13:22:58 -0700

    Update InstallLinuxMIDServer.ps1

--- InstallLinuxMIDServer.ps1
+++ InstallLinuxMIDServer.ps1
@@ -4,8 +4,10 @@
 sudo yum update
 
 #JRE Install 
-sudo yum install java-1.8.0-openjdk
-sudo update-alternatives --config java
+if ((get-item /usr/lib/jvm/jre-1.8.0-openjdk/bin/*).count -eq 0) {
+    sudo yum install java-1.8.0-openjdk
+    .sudo update-alternatives --config java
+}
 
 cp /etc/profile /home/profile_backup
 
@@ -16,7 +18,7 @@ cp /etc/profile /home/profile_backup
 
 $profileTXT  = get-content /etc/profile
 $EOF = $profileTXT.count
-$index = do {$index++ } while ($profileTXT[$index] -ne "export PATH USER LOGNAME MAIL HOSTNAME HISTSIZE HISTCONTROL" -and $index -le $eof)
+do {$index++ } while ($profileTXT[$index] -ne "export PATH USER LOGNAME MAIL HOSTNAME HISTSIZE HISTCONTROL" -and $index -le $eof)
 
 if ($index -lt $EOF) {
     $newProfileTXT = $profileTXT[0..$index]
@@ -26,35 +28,41 @@ if ($index -lt $EOF) {
 }
 
 #Reload profile
-source /etc/profile
+#source /etc/profile
 
 #test 
-echo $JAVA_HOME
+#echo $JAVA_HOME
 
 
 ## MID Server install 
 
 sudo mkdir /opt/midserver
+cd /opt/midserver
 sudo chown snmidserver /opt/midserver
 sudo chgrp snmidserver /opt/midserver
 
-curl https://install.service-now.com/glide/distribution/builds/package/mid/2018/02/26/mid.jakarta-05-03-2017__patch8-02-14-2018_02-26-2018_1106.linux.x86-64.zip --output mid.jakarta-05-03-2017__patch8-02-14-2018_02-26-2018_1106.linux.x86-64.zip
+if (!(test-path mid.jakarta*.zip) {
+    curl https://install.service-now.com/glide/distribution/builds/package/mid/2018/02/26/mid.jakarta-05-03-2017__patch8-02-14-2018_02-26-2018_1106.linux.x86-64.zip --output mid.jakarta-05-03-2017__patch8-02-14-2018_02-26-2018_1106.linux.x86-64.zip
+}
 
 sudo yum install unzip
-unzip mid.jakarta-05-03-2017__patch8-02-14-2018_02-26-2018_1106.linux.x86-64.zip
+
+if (!(test-path /opt/midserver/agent/start.sh)) {
+    unzip -o mid.jakarta-05-03-2017__patch8-02-14-2018_02-26-2018_1106.linux.x86-64.zip -d /opt/midserver
+}
 
 #Modify the config file to connect to our instance. 
 #vi /opt/midserver/agent/config.xml
 $ConfigFile = import-xml /opt/midserver/agent/config.xml
-$InstanceURL = "https://developer12345.service-now.com"
+$InstanceURL = "developer82982.Service-Now.com"
 $MidServerUserName = "Administrator" 
 $MidServerUserPassword = "P@ssword1"
 $MidServerName = [System.Net.DNS]::GetHostByName('').HostName
-$ConfigFile.url = $InstanceURL
-$ConfigFile.mid.instance.username = $MidServerUserName
-$ConfigFile.mid.instance.password = $MidServerUserPassword
-$ConfigFile.name = $MidServerName
-$configfile | export-xml /opt/midserver/agent/config.xml
+$ConfigFile.replace('YOUR_INSTANCE.Service-now.com',$InstanceURL)
+$ConfigFile.replace('YOUR_INSTANCE_USER_NAME_HERE',$MidServerUserName)
+$ConfigFile.replace('YOUR_INSTANCE_PASSWORD_HERE',$MidServerUserPassword)
+$ConfigFile.REPLACE('YOUR_MIDSERVER_NAME_GOES_HERE',$MidServerName)
+$configfile | OUT-FILE /opt/midserver/agent/config.xml
 
 
 # Next you have to add execute permission for two files
